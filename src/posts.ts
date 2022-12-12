669e43604011becde71853fc8aa609588ea6b299
DALHOUM-Ahmed <dalhoum.co@gmail.com>
2022-12-12 10:34:57 +0100

    code publish

---
+++ src/posts.ts
@@ -0,0 +1,72 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  CreatePost as CreatePostEvent,
+  DeletePost as DeletePostEvent,
+  UpdateBody as UpdateBodyEvent,
+  UpdatePost as UpdatePostEvent
+} from "../generated/posts/posts";
+import { User, Post } from "../generated/schema";
+
+export function handleCreatePost(event: CreatePostEvent): void {
+  let entity = new Post(event.params.postID.toString());
+  let user = User.load(event.params._author.toString());
+  if (user !== null) {
+    entity.author = user._userAddress.toString();
+    entity.title = event.params.title;
+    entity.body = event.params.body;
+    entity.createdAt = event.params.createdAt;
+    entity.groupID = event.params.groupID;
+    entity.postID = event.params.postID;
+
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+    entity.deleted = false;
+    entity.editedAt = new BigInt(0);
+    if (user.posts && event.params.postID.toString()) {
+      user.posts.push(event.params.postID.toString());
+    }
+
+    entity.save();
+  }
+}
+
+export function handleDeletePost(event: DeletePostEvent): void {
+  let entity = Post.load(event.params.postID.toString());
+  if (entity !== null) {
+    let user = User.load(entity.author.toString());
+    //should handle executor
+    // entity._executor = event.params._executor;
+
+    if (user !== null) {
+      if (user.posts) {
+        const index = user.posts.indexOf(entity.postID.toString(), 0);
+        if (index > -1) {
+          user.posts = user.posts.splice(index, 1);
+        }
+
+        user.save();
+        entity.deleted = true;
+        entity.save();
+      }
+    }
+  }
+}
+
+export function handleUpdateBody(event: UpdateBodyEvent): void {
+  let entity = Post.load(event.params.postID.toString());
+  if (entity) {
+    entity.body = event.params.body;
+    entity.editedAt = event.block.timestamp;
+    entity.save();
+  }
+}
+
+export function handleUpdatePost(event: UpdatePostEvent): void {
+  let entity = Post.load(event.params.postID.toString());
+  if (entity) {
+    entity.title = event.params.title;
+    entity.editedAt = event.block.timestamp;
+    entity.save();
+  }
+}
--- src/users.ts
+++ src/users.ts
@@ -13,232 +13,145 @@ import {
   SetTelephoneVerifiedData as SetTelephoneVerifiedDataEvent,
   SetUsername as SetUsernameEvent,
   Signup as SignupEvent
-} from "../generated/users/users"
-import {
-  AddTag,
-  SetBio,
-  SetEmail,
-  SetEmailVerifiedData,
-  SetFingerScan,
-  SetFirstName,
-  SetGovID,
-  SetLastName,
-  SetPictureNFT,
-  SetPictureUpload,
-  SetTelephone,
-  SetTelephoneVerifiedData,
-  SetUsername,
-  Signup
-} from "../generated/schema"
+} from "../generated/users/users";
+import { User } from "../generated/schema";
 
-export function handleAddTag(event: AddTagEvent): void {
-  let entity = new AddTag(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.tag = event.params.tag
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+//should add tag entity+update the user contract (can't use string[])
+// export function handleAddTag(event: AddTagEvent): void {
+//   let entity = User.load(event.params._userAddress.toString())
+//   if (entity !== null) {
+//     entity.tag = event.params.tag
+//     entity.save()
+//   }
+// }
 
 export function handleSetBio(event: SetBioEvent): void {
-  let entity = new SetBio(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.bio = event.params.bio
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.bio = event.params.bio;
+    entity.save();
+  }
 }
 
 export function handleSetEmail(event: SetEmailEvent): void {
-  let entity = new SetEmail(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.email = event.params.email
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.email = event.params.email;
+    entity.save();
+  }
 }
 
 export function handleSetEmailVerifiedData(
   event: SetEmailVerifiedDataEvent
 ): void {
-  let entity = new SetEmailVerifiedData(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.data = event.params.data
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.emailVerifiedData = event.params.data;
+    entity.save();
+  }
 }
 
 export function handleSetFingerScan(event: SetFingerScanEvent): void {
-  let entity = new SetFingerScan(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.fingerScan = event.params.fingerScan
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.fingerScan = event.params.fingerScan;
+    entity.save();
+  }
 }
 
 export function handleSetFirstName(event: SetFirstNameEvent): void {
-  let entity = new SetFirstName(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.firstName = event.params.firstName
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.firstName = event.params.firstName;
+    entity.save();
+  }
 }
 
 export function handleSetGovID(event: SetGovIDEvent): void {
-  let entity = new SetGovID(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.govID = event.params.govID
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.govtID = event.params.govID;
+    entity.save();
+  }
 }
 
 export function handleSetLastName(event: SetLastNameEvent): void {
-  let entity = new SetLastName(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.lastName = event.params.lastName
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.lastName = event.params.lastName;
+    entity.save();
+  }
 }
 
 export function handleSetPictureNFT(event: SetPictureNFTEvent): void {
-  let entity = new SetPictureNFT(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.nftAddress = event.params.nftAddress
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.nftAddress = event.params.nftAddress;
+    entity.ownedID = event.params.id;
+    entity.save();
+  }
 }
 
 export function handleSetPictureUpload(event: SetPictureUploadEvent): void {
-  let entity = new SetPictureUpload(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.url = event.params.url
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.pictureUpload = event.params.url;
+    entity.save();
+  }
 }
 
 export function handleSetTelephone(event: SetTelephoneEvent): void {
-  let entity = new SetTelephone(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.telephone = event.params.telephone
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.telephone = event.params.telephone;
+    entity.save();
+  }
 }
 
 export function handleSetTelephoneVerifiedData(
   event: SetTelephoneVerifiedDataEvent
 ): void {
-  let entity = new SetTelephoneVerifiedData(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.data = event.params.data
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.telephoneVerifiedData = event.params.data;
+    entity.save();
+  }
 }
 
 export function handleSetUsername(event: SetUsernameEvent): void {
-  let entity = new SetUsername(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._userAddress = event.params._userAddress
-  entity.username = event.params.username
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = User.load(event.params._userAddress.toString());
+  if (entity !== null) {
+    entity.userName = event.params.username;
+    entity.save();
+  }
 }
 
 export function handleSignup(event: SignupEvent): void {
-  let entity = new Signup(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.userId = event.params.userId
-  entity._userAddress = event.params._userAddress
-  entity.firstName = event.params.firstName
-  entity.lastName = event.params.lastName
-  entity.userName = event.params.userName
-  entity.email = event.params.email
-  entity.bio = event.params.bio
-  entity.pictureUpload = event.params.pictureUpload
-  entity.nftAddress = event.params.nftAddress
-  entity.ownedID = event.params.ownedID
-  entity.telephone = event.params.telephone
-  entity.govtID = event.params.govtID
-  entity.fingerScan = event.params.fingerScan
+  let entity = newUser(event.params._userAddress.toString());
+  entity.userId = event.params.userId;
+  entity._userAddress = event.params._userAddress;
+  entity.firstName = event.params.firstName;
+  entity.lastName = event.params.lastName;
+  entity.userName = event.params.userName;
+  entity.email = event.params.email;
+  entity.bio = event.params.bio;
+  entity.pictureUpload = event.params.pictureUpload;
+  entity.nftAddress = event.params.nftAddress;
+  entity.ownedID = event.params.ownedID;
+  entity.telephone = event.params.telephone;
+  entity.govtID = event.params.govtID;
+  entity.fingerScan = event.params.fingerScan;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.deleted = false;
 
-  entity.save()
+  entity.save();
+}
+
+function newUser(id: string): User {
+  let entity = new User(id);
+
+  return entity;
 }
